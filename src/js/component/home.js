d4061f31b697cd9772d65bc8284404676c06b557
plotier <plotier15@gmail.com>
2021-06-12 17:25:22 +0000

    antes de poner la funcion del contador

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,7 +3,7 @@ import React from "react";
 //create your first component
 export function Home() {
 	return (
-		<div className="counter mt-5 row justify-content-md-center">
+		<div className="counter mt-5 row justify-content-md-center mx-auto">
 			<div className="clock m-5"></div>
 			<div className="col-1">0</div>
 			<div className="col-1">0</div>
