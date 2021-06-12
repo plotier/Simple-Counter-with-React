e8c5dbebc7d52c8cacf35f10b45a47ea6d7a83d0
plotier <plotier15@gmail.com>
2021-06-12 16:45:53 +0000

    base con reloj

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,16 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter mt-5 row justify-content-md-center">
+			<div className="clock m-5"></div>
+			<div className="col-1">0</div>
+			<div className="col-1">0</div>
+			<div className="col-1">0</div>
+			<div className="col-1">0</div>
+			<div className="col-1">0</div>
+			<div className="col-1">0</div>
 		</div>
 	);
 }
